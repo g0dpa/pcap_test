d53fb0f650bee66eccd82243aa80861c508c3bb5
g0dpa <highlight@korea.ac.kr>
2018-09-23 19:18:42 +0900

    Use symbol like IPPROTO_TCP instead of rare 0x06

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdint.h>
 #include <pcap.h>
 #include <arpa/inet.h>
+
+#define ETHERTYPE_IPv4 0x0800
+#define ETHERTYPE_IPv6 0x86DD
+#define ETHERTYPE_ARP  0x0806
 
 struct eth_hdr {
 	uint8_t dest[6];
@@ -25,21 +29,22 @@ struct ipv4_hdr {
 	uint8_t src[4];
 	uint8_t dest[4];
 	uint8_t data[0];
-} __attribute__((packed));
+};
 
-#define TCP_FIND_THL(XX) ((uint8_t)((((uint8_t*)(&(XX)->DRF))[0] & 0xF0) >> 2))
+// THL >> 4(/16) << 5(*32) >> 3(/8)
+#define TCP_FIND_THL(XX) ((uint8_t)((((uint8_t*)(&(XX)->HRF))[0] & 0xF0) >> 2))
 
 struct tcp_hdr {
 	uint16_t src;
 	uint16_t dest;
 	uint32_t seq_number;
 	uint32_t ack_number;
-	uint16_t DRF; // THL, Reserved, Flags
+	uint16_t HRF; // THL, Reserved, Flags
 	uint16_t window_size;
 	uint16_t tcp_checksum;
 	uint16_t urg_pointer;
 	uint8_t payload[0];
-} __attribute__((packed));
+};
 
 void usage() {
   printf("syntax: pcap_test <interface>\n");
@@ -49,12 +54,12 @@ void usage() {
 
 char errbuf[PCAP_ERRBUF_SIZE];
 
-void pk_length(const u_char*, int len);
+void pk_start(const u_char*, int len);
 void pk_eth(const struct eth_hdr* packet_eth);
 void pk_ipv4(const struct ipv4_hdr* packet_ipv4);
 void pk_tcp(const struct tcp_hdr*, uint16_t length);
 
-void pk_payload(const uint8_t*, uint32_t);
+void pk_print_payload(const uint8_t*, uint32_t len);
 
 int main(int argc, char* argv[]){
   /* check there exists argv[1] */
@@ -62,8 +67,9 @@ int main(int argc, char* argv[]){
 
   /* read packet from argv[1] */
   pcap_t* handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
-  if (handle == NULL) {
-    fprintf(stderr, "couldn't open device %s: %s\n", argv[1], errbuf);
+	//pcap_t* handle = pcap_open_offline(argv[1],errbuf);
+	if (handle == NULL) {
+    fprintf(stderr, "Cannot Open Device %s: %s\n", argv[1], errbuf);
     return -1;
   }
 
@@ -75,12 +81,12 @@ int main(int argc, char* argv[]){
     if (res == -1 || res == -2) break;
 
     /* let's start to analyze packet */
-    pk_length(packet, header->caplen);
+    pk_start(packet, header->caplen);
   }
   return 0;
 }
 
-void pk_length(const u_char* packet, int len){
+void pk_start(const u_char* packet, int len){
   /* print length of packet */
 	printf("Packet length: %u\n", len);
 
@@ -104,20 +110,21 @@ void pk_eth(const struct eth_hdr* packet_eth){
   puts("");
 
   switch (ntohs(packet_eth->type)){
-    case 0x0806: // ARP
+    case ETHERTYPE_ARP:
       printf("Ethertype: ARP\n");
       break;
-    case 0x0800: // IPv4
+    case ETHERTYPE_IPv4:
   		printf("Ethertype: IPv4\n");
   		pk_ipv4((const struct ipv4_hdr*)packet_eth->data);
   		break;
-  	case 0x86DD: // IPv6
+  	case ETHERTYPE_IPv6:
   		printf("Ethertype: IPv6\n");
   		break;
   	default:
   		printf("Ethertype: Unknown\n");
   		break;
   }
+	puts("");
 }
 
 /*print Network Layer */
@@ -139,21 +146,21 @@ void pk_ipv4(const struct ipv4_hdr *packet_ipv4){
   /* check option field */
   uint8_t ihl = IPV4_FIND_IHL(packet_ipv4);
   if (ihl < 20) {
-    fprintf(stderr, "IHL is too small to call packet valid");
+    fprintf(stderr, "IHL is too small to call packet valid\n");
   }
   else if (ihl == 20) printf("No option for IPv4\n");
   else printf("IPv4 option. IHL: %d\n", ihl);
 
   /* check Protocol ID */
   switch (packet_ipv4->protocol) {
-  case 6:
+  case IPPROTO_TCP:
     printf("IPv4 protocol ID: TCP\n");
     pk_tcp((const struct tcp_hdr*)&packet_ipv4->data[ihl - 20], ntohs(packet_ipv4->length) - ihl);
     break;
-  case 17:
+  case IPPROTO_UDP:
     printf("IPv4 protocol ID: UDP\n");
     break;
-  case 1:
+  case IPPROTO_ICMP:
     printf("IPv4 protocol ID: ICMP\n");
     break;
   default:
@@ -173,9 +180,9 @@ void pk_tcp(const struct tcp_hdr *packet_tcp, uint16_t length){
 	/* check header length */
 	uint8_t hl = TCP_FIND_THL(packet_tcp);
 	if (hl > 60) {
-    fprintf(stderr, "HL is too big to call packet valid");
+    fprintf(stderr, "HL is too big to call packet valid\n");
   } else if (hl < 20) {
-    fprintf(stderr, "HL is too small to call packet valid");
+    fprintf(stderr, "HL is too small to call packet valid\n");
   }
 
 	/* print data length */
@@ -187,11 +194,11 @@ void pk_tcp(const struct tcp_hdr *packet_tcp, uint16_t length){
 
 	/* prints payload as hexademical */
 	printf("Payload(Cut by 16Byte): ");
-	pk_payload(&packet_tcp->payload[hl - 20], len);
+	pk_print_payload(&packet_tcp->payload[hl - 20], len);
 }
 
 /* print payload as hexademical */
-void pk_payload(const uint8_t *Data, uint32_t len) {
+void pk_print_payload(const uint8_t *Data, uint32_t len) {
 	for (uint32_t i = 0; i < len; i++) {
 		printf("%s%02X", (i > 0 ? " " : ""), Data[i]);
 	}
